c6854e3a76d5f8165575d081e2d81c89e020396f
Bruce Tan <bruce.tan@displayr.com>
2025-03-11 18:18:51 +1300

    Update package.nix
    Co-authored-by: Jordan Cran <jordancran@hey.com>

--- package.nix
+++ package.nix
@@ -4,7 +4,8 @@ pkgs.rPackages.buildRPackage {
   name = "fastrtext";
   version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
   src = ./.;
-  description = ''Learning text representations and text classifiers may rely
+  description = ''
+    Learning text representations and text classifiers may rely
     on the same simple and efficient approach. 'fastText' is an open-source, free, 
     lightweight library that allows users to perform both tasks.
     It transforms text into continuous vectors that can later
@@ -12,7 +13,9 @@ pkgs.rPackages.buildRPackage {
     It works on standard, generic hardware (no 'GPU' required).
     It also includes model size reduction feature.
     'fastText' original source code is available 
-    at <https://github.com/facebookresearch/fastText>.'';
+    at <https://github.com/facebookresearch/fastText>.
+  '';
+
   propagatedBuildInputs = with pkgs.rPackages; [ 
     Rcpp
     assertthat
